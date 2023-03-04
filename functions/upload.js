dae13a12bc327f2131e323fdae559d8d50f5fc05
mydracula <1462465969@qq.com>
2023-03-05 02:43:36 +0800

    ?

--- functions/upload.js
+++ functions/upload.js
@@ -1,19 +1,18 @@
 export async function onRequestPost(context) {
-  const { request } = context
+  // Contents of context object
+  const {
+    request, // same as existing Worker API
+    env, // same as existing Worker API
+    params, // if filename includes [id] or [[path]]
+    waitUntil, // same as ctx.waitUntil in existing Worker API
+    next, // used for middleware or to fetch assets
+    data // arbitrary space for passing data between middlewares
+  } = context
   const url = new URL(request.url)
   const response = fetch('https://telegra.ph/' + url.pathname + url.search, {
     method: request.method,
+    headers: request.headers,
     body: request.body
   })
-  // const headers = new Headers(response.headers)
-  // headers.set('Access-Control-Allow-Origin', '*')
-  // headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT')
-  // headers.set('Access-Control-Allow-Headers', '*')
-  // headers.set('Access-Control-Allow-Credentials', 'true')
-  // 将服务端的响应返回给客户端
-  return new Response(response.body, {
-    status: response.status,
-    statusText: response.statusText,
-    headers: headers
-  })
+  return response
 }
