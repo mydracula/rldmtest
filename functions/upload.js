0f8df73b22779b233e0dfa1250e4e7597bcf1503
mydracula <1462465969@qq.com>
2023-03-05 04:40:35 +0800

    ?

--- functions/upload.js
+++ functions/upload.js
@@ -1,7 +1,7 @@
 export async function onRequestPost(context) {
   const { request } = context
   const url = new URL(request.url)
-  const response = fetch('https://telegra.ph/' + url.pathname + url.search, {
+  const response = fetch('https://telegra.ph/' + url.pathname, {
     method: request.method,
     headers: request.headers,
     body: request.body
